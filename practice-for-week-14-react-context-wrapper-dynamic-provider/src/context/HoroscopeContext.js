dc949d97f0e471faec0dcdc3120f5f0fb76d8d85
George Merida <gmerida92@gmail.com>
2022-09-07 20:57:26 -0700

    Practice context wrapper provider complete

--- practice-for-week-14-react-context-wrapper-dynamic-provider/src/context/HoroscopeContext.js
+++ practice-for-week-14-react-context-wrapper-dynamic-provider/src/context/HoroscopeContext.js
@@ -1,3 +1,19 @@
 import { createContext } from 'react';
+import { useState } from 'react';
+import horoscopesObj from '../data/horoscopes';
 
 export const HoroscopeContext = createContext();
+
+function HoroscopeProvider(props) {
+
+    const [currentSign, setCurrentSign] = useState('Aquarius');
+    const sign = horoscopesObj[currentSign];
+
+    return (
+        <HoroscopeContext.Provider value ={{sign, setCurrentSign}}>
+            {props.children}
+        </HoroscopeContext.Provider>
+    );
+};
+
+export default HoroscopeProvider;
