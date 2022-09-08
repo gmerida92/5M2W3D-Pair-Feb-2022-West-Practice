de406d077beb416b248440e5a5bdf7f5ec3001b3
Kelly <kelly.a.artola@ricealumni.net>
2022-09-08 00:06:33 -0500

    refresh

--- practice-for-week-14-react-context-wrapper-dynamic-consumer/src/components/Navbar.js
+++ practice-for-week-14-react-context-wrapper-dynamic-consumer/src/components/Navbar.js
@@ -1,17 +1,21 @@
-import horoscopesObj from '../data/horoscopes';
+import horoscopesObj from "../data/horoscopes";
+import { useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
 
 const Navbar = () => {
   const horoscopes = Object.keys(horoscopesObj);
+  const { setCurrentSign } = useContext(HoroscopeContext);
+  console.log("rerenders");
 
   return (
     <nav>
-      {horoscopes.map(sign => (
-        <span key={sign}>
-        {sign}
+      {horoscopes.map((sign) => (
+        <span key={sign} onClick={() => setCurrentSign(sign)}>
+          {sign}
         </span>
       ))}
     </nav>
-  )
+  );
 };
 
 export default Navbar;
